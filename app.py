aa6d27f20525ed33a602cecc83f15c5ccf03e6ce
Anuraga Sahoo <anuragliku.2020@gmail.com>
2025-04-25 16:11:48 +0530

    logo updated

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def calculate():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
